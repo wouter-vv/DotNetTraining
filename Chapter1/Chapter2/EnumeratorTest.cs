08e0ed2480542427aa94eb56bc7610bd74543bb6
Wouter Vande Velde <Wouter.VandeVelde@codit.eu>
2019-12-10 18:57:21 +0100

    Test enumerator

---
+++ Chapter1/Chapter2/EnumeratorTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter2
+{
+    class EnumeratorTest : IEnumerator<int>, IEnumerable
+    {
+
+        int count;
+        int limit;
+
+        public int Current
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            if (++count == limit)
+                return false;
+            else
+                return true;
+        }
+
+        public void Reset()
+        {
+            count = 0;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+
+        public EnumeratorTest(int limit)
+        {
+            count = 0;
+            this.limit = limit;
+        }
+
+    }
+}
