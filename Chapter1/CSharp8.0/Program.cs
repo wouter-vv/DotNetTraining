a6ef8e8264ad9468a41a8882daa40b2d1a3589fb
Wouter Vande Velde <wouter.vandevelde@outlook.com>
2020-03-26 21:23:10 +0100

    Add C# 8.0 switch statement

---
+++ Chapter1/CSharp8.0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp8._0
+{
+    class Program
+    {
+        public static string RockPaperScissors(string first, string second)
+            => (first, second) switch
+            {
+                ("rock", "paper") => "rock is covered by paper. Paper wins.",
+                ("rock", "scissors") => "rock breaks scissors. Rock wins.",
+                ("paper", "rock") => "paper covers rock. Paper wins.",
+                ("paper", "scissors") => "paper is cut by scissors. Scissors wins.",
+                ("scissors", "rock") => "scissors is broken by rock. Rock wins.",
+                ("scissors", "paper") => "scissors cuts paper. Scissors wins.",
+                (_, _) => "tie"
+            };
+}
+}
---
+++ Chapter1/CSharp8.0/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Chapter4;
+using CSharp8._0;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+
+            Program p = new Program();
+
+            p.TuplePatterns();
+
+
+        }
+    }
+}
